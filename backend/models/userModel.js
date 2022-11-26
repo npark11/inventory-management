ad28a851e030b1d74fa204fe01a64613793269b9
Nari Park <npark11@myseneca.ca>
2022-11-26 13:18:09 -0500

    created user route & controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -8,7 +9,7 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please enter your email"],
-    unique: treu,
+    unique: true,
     trim: true,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
@@ -17,9 +18,9 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    required: [ture, "Please enter your password"],
+    required: [true, "Please enter your password"],
     minLength: [6, "Password must be up to 6 characters"],
-    maxLength: [23, "Password must not be more than 23 characters"]
+    // maxLength: [23, "Password must not be more than 23 characters"]
   },
   photo: {
     type: String,
@@ -37,6 +38,19 @@ const userSchema = mongoose.Schema({
   }
 }, {
   timestamps: true
+});
+
+// Encrypt password before saving to DB
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const userRoute = require('./routes/userRoute');
+const errorHandler = require('./middleWare/errorMiddleware');
 
 const app = express();
 
@@ -11,14 +13,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Routes Middleware
+app.use('/api/users', userRoute);
+
 // Routes
 app.get('/', (req, res) => {
   res.send("Home Page");
 });
 
-const PORT = process.env.PORT || 5000;
+// Error Middleware
+app.use(errorHandler);
 
 // Connect to DB and start server
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
