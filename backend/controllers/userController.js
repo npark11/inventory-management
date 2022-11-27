52f3bf7e88f3021c8512033c057b3dede9f9834d
Nari Park <npark11@myseneca.ca>
2022-11-27 11:11:15 -0500

    added login&logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-
+// Generate Token
 const generateToken = (id) => {
   return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"});
 };
 
+// Register User
 const registerUser = asyncHandler( async (req, res) => {
   const {name, email, password} = req.body;
 
@@ -38,7 +40,7 @@ const registerUser = asyncHandler( async (req, res) => {
   const token = generateToken(user._id);
 
   // Send HTTP-only cookie
-  res.cookie("token", token, {
+  res.cookie("accessToken", token, {
     path: '/',
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400),    // 1day
@@ -50,12 +52,68 @@ const registerUser = asyncHandler( async (req, res) => {
     const { _id, name, email, photo, phone, bio } = user
     res.status(201).json({
       _id, name, email, photo, phone, bio, token
-    })
+    });
   } else {
-    res.status(400)
-    throw new Error("Invalid user data")
+    res.status(400);
+    throw new Error("Invalid user data");
   };
 
 });
 
-module.exports = { registerUser };
+// Login User
+const loginUser = asyncHandler( async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validate Request
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please enter email and password");
+  };
+
+  // Check if user exits
+  const user = await User.findOne({email});
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, please signup");
+  };
+
+  // User exists, check if password is correct
+  const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+  // Generate Token
+  const token = generateToken(user._id);
+
+  // Send HTTP-only cookie
+  res.cookie("accessToken", token, {
+    path: '/',
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400),    // 1day
+    sameSite: "none",
+    secure: true
+  });
+
+  if (user && passwordIsCorrect) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id, name, email, photo, phone, bio, token
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
+});
+
+// Logout User
+const logout = asyncHandler( async (req, res) => {
+  res.cookie("accessToken", "", {
+    path: '/',
+    httpOnly: true,
+    expires: new Date(0),    // 1day
+    sameSite: "none",
+    secure: true
+  });
+  
+  return res.status(200).json({message: "Successfully Logged Out"});
+});
+
+module.exports = { registerUser, loginUser, logout };
