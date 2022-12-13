514685faa328d4f235d88d92b83a6586425493a6
Nari Park <npark11@myseneca.ca>
2022-12-13 13:24:53 +0900

    added get all products & single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -49,13 +49,27 @@ const createProduct =  asyncHandler ( async (req, res) => {
 
 });
 
+// Get all Products
+const getProducts =  asyncHandler ( async (req, res) => {
+  const products = await Product.find({user: req.user.id}).sort("-createdAt");
+  res.status(200).json(products);
+});
 
+// Get single product
+const getProduct = asyncHandler ( async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404)
+    throw new Error("Product not found")
+  }
 
-
-// const createProduct =  asyncHandler ( async (req, res) => {
+  if (product.user.toString() !== req.user.id) {
+    res.status(404)
+    throw new Error("User not authorized")
+  }
   
-// });
+  res.status(200).json(product);
+});
 
 
-
-module.exports = { createProduct };
+module.exports = { createProduct, getProducts, getProduct };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middleWare/authMiddleware');
-const { createProduct } = require('../controllers/productController');
+const { createProduct, getProducts, getProduct } = require('../controllers/productController');
 const { upload } = require('../utils/fileUpload');
 
 
 router.post('/', protect, upload.single("image"), createProduct);
+router.get('/', protect, getProducts);
+router.get('/:id', protect, getProduct);
 
 
 module.exports = router;
