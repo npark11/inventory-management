d65a02888d8a53b140a8fa5e79d73e89fd65d996
Nari Park <npark11@myseneca.ca>
2022-12-15 14:45:20 +0900

    created reset password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
       <Route path='/forgot' element={<Forgot />} />
-      <Route path='/resetpassword:resetToken' element={<Reset />} />
+      <Route path='/resetpassword/:resetToken' element={<Reset />} />
       </Routes>
     </BrowserRouter>
   );
