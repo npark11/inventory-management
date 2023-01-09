3c4afd9b5b1122b7fd7f1b08630cdb0545f1b3c0
Nari Park <npark11@myseneca.ca>
2023-01-09 10:20:00 -0500

    created login & logout

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { logoutUser } from '../../services/authService';
+import { SET_LOGIN } from '../../redux/features/auth/authSlice'
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(SET_LOGIN(false));
+    navigate('/login');
+  };
+
   return (
     <div className='--pad header'>
       <div className='--flex-between'>
@@ -8,7 +21,7 @@ const Header = () => {
           <span className='--fw-thin'>Welcome,</span>
           <span className='--color-danger'>Nari </span>
         </h3>
-        <button className='--btn --btn-danger'>Logout</button>
+        <button onClick={logout} className='--btn --btn-danger'>Logout</button>
       </div>
     </div>
   )
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -7,6 +7,7 @@ export const validateEmail = (email) => {
   return email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 };
 
+// Register User
 export const registerUser = async (userData) => {
   try {
     const response = await axios.post(`${BACKEND_URL}/api/users/register`, userData, {withCredentials: true})
@@ -22,5 +23,37 @@ export const registerUser = async (userData) => {
     ) || error.message || error.toString();
     toast.error(message);
     console.log(error);
+  }
+};
+
+// Login User
+export const loginUser = async (userData) => {
+  try {
+    const response = await axios.post(`${BACKEND_URL}/api/users/login`, userData, {withCredentials: true})
+    if (response.statusText === "OK") {
+      toast.success("Login successful...");
+    }
+
+    return response.data
+
+  } catch (error) {
+    const message = (
+      error.response && error.response.data && error.response.data.message
+    ) || error.message || error.toString();
+    toast.error(message);
+    console.log(error);
+  }
+};
+
+// Logout User
+export const logoutUser = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/users/logout`);
+    toast.success(response.data.message);
+  } catch (error) {
+    const message = (
+      error.response && error.response.data && error.response.data.message
+    ) || error.message || error.toString();
+    toast.error(message);
   }
 };
